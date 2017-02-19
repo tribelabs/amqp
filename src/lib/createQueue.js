0acd26eabb310afb6ef736e41b50f79a086db211
jsifalda <sifalda.jiri@gmail.com>
2017-02-19 17:59:41 +0700

    Ability to pass opts

--- src/lib/createQueue.js
+++ src/lib/createQueue.js
@@ -1,14 +1,14 @@
 var create = require('./channel/create.js')
 
 module.exports = (storage, connect, debug) => {
-  var createQueue = (queue) => {
+  var createQueue = (queue, opts) => {
     var channel = storage(queue)
 
     if (!channel) {
       debug('Create channel queue for', queue)
       channel = create(connect)
       .then((channel) => {
-        return channel.assertQueue(queue)
+        return channel.assertQueue(queue, opts || {})
         .then(() => {
           return channel
         })
