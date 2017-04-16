932b4d1b066ddb3e1e5482bef0a5dca2e694ce4e
jsifalda <sifalda.jiri@gmail.com>
2017-04-16 15:00:22 +0800

    Better syntax

--- src/lib/utils/buildConsumeCallback.js
+++ src/lib/utils/buildConsumeCallback.js
@@ -5,12 +5,14 @@ module.exports = (queue, callbacks, debug) => {
   return function () {
     var args = arguments
 
+    callbacks = callbacks.map((callback) => {
+      return apply(callback, ...args)
+    })
+
     return new Promise((resolve, reject) => {
       debug('Running consume callbacks for:', queue, '#' + callbacks.length)
 
-      lasync.waterfall(callbacks.map((callback) => {
-        return apply(callback, ...args)
-      }), (error, result) => {
+      lasync.waterfall(callbacks, (error, result) => {
         if (error) {
           console.warn('Error occurred in queue "' + queue + '" middleware:', error)
           if (error.stack) {
