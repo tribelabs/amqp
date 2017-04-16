69c5893468b8b974a5c5ae2ecf743ad362d7793e
jsifalda <sifalda.jiri@gmail.com>
2017-04-16 16:19:40 +0800

    Remove prefetch debug

--- src/lib/channel/prefetch.js
+++ src/lib/channel/prefetch.js
@@ -1,7 +1,6 @@
 module.exports = (debug) => {
   return (channel, value) => {
     var prefetch = value === true ? 1 : Number(value)
-    debug('Prefetch channel with', prefetch)
 
     return channel.prefetch(prefetch)
     .then(() => {
