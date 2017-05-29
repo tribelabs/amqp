9ec2469cf3aba055c0252aa535dfdccb72ffcbee
jsifalda <sifalda.jiri@gmail.com>
2017-05-29 15:25:46 +0100

    Refactoring of publish method

--- src/lib/publish.js
+++ src/lib/publish.js
@@ -21,41 +21,49 @@ module.exports = (createQueue, debug) => {
     }
   }
 
+  var createQueueForAnswer = (channel, consumer) => {
+    debug('Creating queue for answer on...')
+
+    return channel.assertQueue('', {
+      exclusive: true, // do not allow others to use this queue
+      autoDelete: true // the queue will be deleted when the number of consumers drops to zero
+    })
+    .then((replyChannel) => {
+      return channel.consume(replyChannel.queue, consumer)
+      .then(() => {
+        return replyChannel.queue
+      })
+    })
+  }
+
+  var createQueueForAnswerIfNeed = (channel, callback, opts) => {
+    var listenForReply = typeof callback === 'function'
+    var corrId = uuid()
+
+    var promise = Promise.resolve(null)
+
+    if (listenForReply) {
+      promise = createQueueForAnswer(channel, maybeAnswer(channel, corrId, callback, opts.autoDeleteCallback))
+    }
+
+    return promise.then((replyChannel) => {
+      return {
+        channel: channel,
+        replyTo: replyChannel,
+        correlationId: corrId
+      }
+    })
+  }
+
   return function (queue, message, callback, opts) {
     opts = Object.assign({}, defaults, (opts || {}))
-    var listenForReply = typeof callback === 'function'
 
     debug('Should be published', queue)
 
     return new Promise(function (resolve, reject) {
       createQueue(queue)
       .then((channel) => {
-        var corrId = uuid()
-
-        if (listenForReply) {
-          return channel.assertQueue('', {
-            exclusive: true, // do not allow others to use this queue
-            autoDelete: true // the queue will be deleted when the number of consumers drops to zero
-          })
-          .then((replyChannel) => {
-            var consumer = maybeAnswer(channel, corrId, callback, opts.autoDeleteCallback)
-
-            return channel.consume(replyChannel.queue, consumer)
-            .then(() => {
-              return {
-                channel: channel,
-                replyTo: replyChannel.queue,
-                correlationId: corrId
-              }
-            })
-          })
-        }
-
-        return {
-          channel: channel,
-          replyTo: null,
-          correlationId: corrId
-        }
+        return createQueueForAnswerIfNeed(channel, callback, opts)
       })
       .then((results) => {
         debug('Publish', queue, message, opts)
